f3db2a77c43faca57e213e6a9c1f75024c3a0cb3
Alexei Dias <alexeidias@gmail.com>
2020-03-23 13:24:40 -0700

    nav links hidden

--- segurosiframe/src/components/NavigationBar.js
+++ segurosiframe/src/components/NavigationBar.js
@@ -23,17 +23,17 @@ export default class NavigationBar extends Component {
         return (
             <Styles>
                 <Navbar expand='lg'>
-                   <Navbar.Brand href='/'>Little Start Day Care</Navbar.Brand> 
+                   <Navbar.Brand href='/'>Sinistro finder</Navbar.Brand> 
                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
                    <Navbar.Collapse id='basic-navbar-nav'>
-                       <Nav className='ml-auto'>
-                           {/* <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item> */}
+                       {/* <Nav className='ml-auto'>
+                           <Nav.Item><Nav.Link href='/'>Home</Nav.Link></Nav.Item>
                            <Nav.Item><Nav.Link href='/about'>About</Nav.Link></Nav.Item>
                            <Nav.Item><Nav.Link href='/specialEv'>Special Events</Nav.Link></Nav.Item>
                            <Nav.Item><Nav.Link href='/calendar'>Calendar</Nav.Link></Nav.Item>
                            <Nav.Item><Nav.Link href='/rules'>Rules</Nav.Link></Nav.Item>
                            <Nav.Item><Nav.Link href='/parentArea'>Login</Nav.Link></Nav.Item>
-                       </Nav>
+                       </Nav> */}
                    </Navbar.Collapse>
                 </Navbar>
             </Styles>
